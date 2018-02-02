b15af1ac5058e49569f84fa247b89281536e7128
Ryan Ghaida <ryaboug@gmail.com>
2018-02-02 22:26:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function happyHolidayTo(holiday, name) {
 
 function holidayCountdown(days, holiday) {
   
-  return `It's ${days} days until ${holiday}!`
+  return `It's  days until ${holiday}${days}!`
 }
